1a4aaed24628a806c445a6f5fdbba295824cd25c
Saitore973 <saitorevg@gmail.com>
2022-04-03 23:07:28 +0300

    import posts ro the admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Image, Comment, Follow
+from .models import Profile, Post, Comment, Follow
 # Register your models here.
 
 admin.site.register(Profile)
-admin.site.register(Image)
+admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Follow)
