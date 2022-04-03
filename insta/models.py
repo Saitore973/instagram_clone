882da1ef97a858c761d875cbea52c79ee97fb641
Saitore973 <saitorevg@gmail.com>
2022-04-03 08:00:02 +0300

    create image model

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,7 @@
+from re import M
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +11,13 @@ class Profile(models.Model):
     posts = models.IntegerField(default=0)
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to= 'images/', default="Image")
+    image_name= models.CharField(max_length=44)
+    image_caption= models.TextField()
+    likes = models.IntegerField(default="0")
+    comments = models.TextField(default="No Comment")
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
--- instagram/settings.py
+++ instagram/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta',
-    'bootstrap3'
+    'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
