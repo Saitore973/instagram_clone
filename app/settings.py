5b39cd1605f597524be43dd3165caa41fe9fce32
Saitore973 <saitorevg@gmail.com>
2022-04-05 11:04:48 +0300

    add login image

--- app/settings.py
+++ app/settings.py
@@ -185,3 +185,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL='home'
+REGISTER_REDIRECT_URL = 'home'
+SIMPLE_BACKEND_REDIRECT_URL = 'home'
